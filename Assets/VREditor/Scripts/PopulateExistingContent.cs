9758b791b3e90a5715e20d65c2759d1aa8902a77
DESKTOP-VJFN6PJ\mike_ <mike_griffin@hotmail.co.uk>
2017-10-12 15:15:36 +0100

    Version 1

---
+++ Assets/VREditor/Scripts/PopulateExistingContent.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PopulateExistingContent : MonoBehaviour
+{
+    List<GameObject> myObjects;
+    public GameObject prefabButton;
+    void Start()
+    {
+        myObjects = new List<GameObject>();
+        //Populate();
+    }
+
+    private void OnEnable()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+
+    public void Populate()
+    {
+
+        for (int i = 0; i < myObjects.Count; i++)
+        {
+            Destroy(myObjects[i]);
+        }
+        myObjects.Clear();
+
+        for (int i = 0; i < StateManager.Instance.unityGameObjects.Count; i++)
+        {
+            myObjects.Add(Instantiate(prefabButton, transform));
+            myObjects[i].name = "" + i;
+            myObjects[i].transform.GetChild(0).GetComponent<Text>().text = "" + StateManager.Instance.unityGameObjects[i].name;
+        }
+    }
+}
---
+++ Assets/VREditor/Scripts/PopulatePrefabsContent.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PopulatePrefabsContent : MonoBehaviour
+{
+    public GameObject prefabButton;
+    List<GameObject> myObjects;
+    void Start()
+    {
+        myObjects = new List<GameObject>();
+    }
+
+    private void OnEnable()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+
+    public void Populate()
+    {
+
+        for (int i = 0; i < myObjects.Count; i++)
+        {
+            Destroy(myObjects[i]);
+        }
+        myObjects.Clear();
+
+        for (int i = 0; i < StateManager.Instance.prefabsFromFolder.Length; i++)
+        {
+            // Create new instances of our prefab until we've created as many as we specified
+            myObjects.Add(Instantiate(prefabButton, transform));
+            myObjects[i].name = "" + i;
+            myObjects[i].transform.GetChild(0).GetComponent<Text>().text = "" + StateManager.Instance.prefabsFromFolder[i].name;
+        }
+    }
+}
